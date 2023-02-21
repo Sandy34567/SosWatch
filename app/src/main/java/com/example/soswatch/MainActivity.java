3d9142a359cf2c72b32cced2332849b7cf1b18f6
s-arslaan <shaikharslaan1234@gmail.com>
2023-02-21 22:09:21 +0530

    added google sign in with api

--- app/src/main/java/com/example/soswatch/MainActivity.java
+++ app/src/main/java/com/example/soswatch/MainActivity.java
@@ -2,15 +2,17 @@ package com.example.soswatch;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.icu.text.IDNA;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.soswatch.databinding.ActivityMainBinding;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
@@ -18,6 +20,11 @@ import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends Activity {
 
@@ -46,6 +53,8 @@ public class MainActivity extends Activity {
                 Intent intent = mSignInClient.getSignInIntent();
                 startActivityForResult(intent, RC_SIGN_IN);
 
+//                mSignInClient.signOut();
+
             }
         });
 
@@ -64,11 +73,50 @@ public class MainActivity extends Activity {
                 // Get account information.
                 String fullName = acct.getDisplayName();
                 String givenName = acct.getGivenName();
+                String idToken = acct.getIdToken();
                 String familyName = acct.getFamilyName();
-                String email = acct.getEmail();
+                String id = acct.getId();
+                String email = acct.getEmail();//110022754939632693383
 
-                Log.d("gsign:",fullName);
+                Log.d("gsign:",fullName+" ,"+givenName+" , "+familyName+" ,"+idToken+" , "+id+" , "+email);
+
+                storeBeachUser(fullName, email, familyName, id);
             }
         }
+    }
+
+    private void storeBeachUser(String name, String email, String familyName, String id) {
+        String url = "https://app.arslaan.link/api/googleSignIn";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+
+        Map<String, String> params = new HashMap();
+        params.put("token",id);
+        params.put("name",name);
+        params.put("familyName",familyName);
+        params.put("email",email);
+
+        JSONObject parameters = new JSONObject(params);
+
+        RequestQueue req = Volley.newRequestQueue(MainActivity.this);
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.POST, url, parameters, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONObject obj = new JSONObject(String.valueOf(response));
+                    String r_msg = obj.getString("msg");
+                    Log.d("ars", response.toString());
+                    Toast.makeText(MainActivity.this, r_msg, Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }}, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }}) {};
+        requestQueue.add(stringRequest);
+        stringRequest.setShouldCache(false);
     }
 }
